94ba33939f5937b409a7c0c77d85f5fe0e920fd9
Haley Warson <haleywarson@gmail.com>
2021-07-08 20:17:24 -0600

    Complete

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,21 +1,29 @@
 function findShortestStringRecursive(arr) {
-  // type your code here
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  const result = findShortestStringRecursive(arr.slice(1));
+
+  return arr[0].length <= result.length ? arr[0] : result;
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestStringRecursive(['aaa', 'a', 'bb', 'ccc']));
+  console.log("=>", findShortestStringRecursive(["aaa", "a", "bb", "ccc"]));
 
   console.log("");
 
   console.log("Expecting: 'hi'");
-  console.log("=>", findShortestStringRecursive(['cat', 'hi', 'dog', 'an']));
+  console.log("=>", findShortestStringRecursive(["cat", "hi", "dog", "an"]));
 
   console.log("");
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestStringRecursive(['flower', 'juniper', 'lily', 'dandelion']));
+  console.log(
+    "=>",
+    findShortestStringRecursive(["flower", "juniper", "lily", "dandelion"])
+  );
 }
 
 module.exports = findShortestStringRecursive;
